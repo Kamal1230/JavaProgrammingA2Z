9c6f841a96cd729b7a3b55554871a2a27fda699d
Kamal1230 <hossainkamal313@gmail.com>
2018-10-02 10:28:23 -0900

    Done Java

---
+++ src/test/java/aBdFruits/BdFruits.java
@@ -0,0 +1,13 @@
+package aBdFruits;
+
+public class BdFruits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Bangladeshi delicious food is world famous and i like it very much ");
+
+	}
+
+}
